83c39993a85f2a781e076b0137633ee2d7fb7e0d
Abishethvarman V <abishethvarman@gmail.com>
2022-03-28 01:08:18 +0530

    Co-authored-by: Kugan Priyan <kugan7370@users.noreply.github.com>

--- Screens/TrashList.js
+++ Screens/TrashList.js
@@ -1,67 +1,68 @@
 import { View, Text, StatusBar,StyleSheet, TouchableOpacity,Image,ScrollView, Button } from 'react-native';
 import React,{useEffect,useState} from 'react';
 import { Ionicons, AntDesign } from '@expo/vector-icons'; 
-import { collection, onSnapshot,query ,where} from '@firebase/firestore'
+import { collection, onSnapshot,query ,where, doc} from '@firebase/firestore'
 import { db } from '../firebase'
 import {useRoute} from '@react-navigation/native'
 
 const TrashList = ({ navigation}) => {
     const [Tlist, setTlist] = useState()
-    
+   
 
     const route = useRoute();
-    const {uid} = route.params;
-    // console.log(uid);
+    const {spot} = route.params;
+    console.log(spot);
 
     
-    const getTrashSpot = () =>
-    {
-        const ref = collection(db, 'spots')
-        const q = query(ref,where("uid","==",uid))
+    // const getTrashSpot = () =>
+    // {
+    //     const ref = collection(db,'spots')
+    //     // const q = query(ref,where("uid","==",spotid))
 
-        onSnapshot(q, (snapshot) =>
-        {
-            snapshot.docs.map((doc)=>{
-                setTlist({...doc.data(),id:doc.id})
-            })
-            // setTlist((snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()} ))))
+    //     onSnapshot(ref, (snapshot) =>
+    //     {
+    //         snapshot.docs.map((doc)=>{
+    //             setTlist({...doc.data(),id:doc.id})
+    //         })
+    //         // setTlist((snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()} ))))
       
-        })
+    //     })
 
         
-        // console.log(place_name)
-    }
+    //     // console.log(place_name)
+    // }
 
-    useEffect(() => {
-        getTrashSpot();
-    },[])
+    // useEffect(() => {
+    //     getTrashSpot();
+    // },[])
 
     return (
         <View style={Styles.container}>
             <TouchableOpacity onPress={() => navigation.goBack()}>
             <Ionicons style={{paddingTop:10}} name="chevron-back" size={30} color="#4c4c4b" />
             </TouchableOpacity>
-  
-            {Tlist && <View style={Styles.headerWrapper}>
-                <Text >{Tlist.place} thani detail screen</Text>
-                <Image source={{uri:Tlist.titleImage}} style={{height:100, width:100}}/>
-                <Text>Date added {Tlist.CreateAt}</Text>
-                <Text >{Tlist.usermail}</Text>
-                <Text>{Tlist.seviority}</Text>
-                <Text>Food_Wrappers {Tlist.Food_Wrappers}</Text>
-                <Text>Polythene_bags {Tlist.Polythene_bags}</Text>
-                <Text>PET_Bottles {Tlist.PET_Bottles}</Text>
-                <Text>Plastic_Debris {Tlist.Plastic_Debris}</Text>
-                <Text>Large_Plastic_Rigid {Tlist.Large_Plastic_Rigid}</Text>
+             
+            {spot &&  <View style={Styles.headerWrapper}>
+                <Text>{spot.place} thani detail screen</Text>
+                <Image source={{uri:spot.titleImage}} style={{height:100, width:100}}/>
+                <Text>Date added {spot.CreateAt}</Text>
+                <Text >{spot.usermail}</Text>
+                <Text>{spot.seviority}</Text>
+                <Text>Food_Wrappers {spot.Food_Wrappers}</Text>
+                <Text>Polythene_bags {spot.Polythene_bags}</Text>
+                <Text>PET_Bottles {spot.PET_Bottles}</Text>
+                <Text>Plastic_Debris {spot.Plastic_Debris}</Text>
+                <Text>Large_Plastic_Rigid {spot.Large_Plastic_Rigid}</Text>
                 <View style={{bottom:0, marginBottom:5}}>
                 <Button title='Im cleaning it on ' color='blue'/>
                 </View>
-            </View>}
+            </View>
+        }
                 
             <View >
   
                 {/* <ScrollView>
-                    {Tlist && Tlist.map((doc,index) => (
+                    {spot && Tlist.map((doc,index) => (
                         
                         <TouchableOpacity key={index} onPress={() => {
                             navigation.navigate('HomeScreen',
