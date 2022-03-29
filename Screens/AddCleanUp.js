7908560491e74df990d72815f5d2d1e716bd8784
Abishethvarman V <abishethvarman@gmail.com>
2022-03-30 00:44:08 +0530

    addDoc

--- Screens/AddCleanUp.js
+++ Screens/AddCleanUp.js
@@ -2,13 +2,48 @@ import { View, Text, StyleSheet, TouchableOpacity, Image, Button } from 'react-n
 import React from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { Ionicons } from '@expo/vector-icons';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../firebase';
  
 
 const AddCleanUp = () => {
   const route = useRoute();
   const {spot} = route.params;
   console.log(spot);
+  console.log(spot.place)
   const navigation = useNavigation();
+
+  const AddSubmit = async () => {
+   
+    await addDoc(collection(db,'cleanup'), {
+        caption: spot.caption,
+        place: spot.placespot,
+        // seviority,
+        // Polythene_bags: counter,
+        // PET_Bottles: counter1,
+        // Plastic_Debris: counter2,
+        // Food_Wrappers: counter3,
+        // Large_Plastic_Rigid: counter4,
+        // createAt: new Date(),
+        // titleImage: ImgUrl,
+        // userid: auth.currentUser.uid,
+        // //username: users.username,
+        // usermail: auth.currentUser.email,
+        // latitude: latitude,
+        // longitude:longitude
+        // // username:auth.currentUser.username
+        
+       
+        
+
+    }).then(() => {
+        Alert.alert('Successfully Added');
+        navigation.push('HomeScreen');
+    })
+
+}
+
+  
   return (
     <View>
       <Header navigation={navigation}/>
@@ -25,7 +60,10 @@ const AddCleanUp = () => {
                 <Text>Plastic_Debris {spot.Plastic_Debris}</Text>
                 <Text>Large_Plastic_Rigid {spot.Large_Plastic_Rigid}</Text>
                 <View style={{bottom:0, marginBottom:5}}>
-                <Button title='Im cleaning it on ' color='green' onPress={()=>navigation.navigate('AddCleanUp',{spot})}/>
+                <Button title='Im cleaning it on ' color='green' onPress={()=>{
+                  AddSubmit();
+                  // DeleteSpot();
+                }}/>
                 </View>
             </View>
     } 
