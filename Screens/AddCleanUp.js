c7a26ef8c94da91820c55d514f04201facf19f40
Abishethvarman V <abishethvarman@gmail.com>
2022-03-30 00:19:14 +0530

    add cleanup scren

--- Screens/AddCleanUp.js
+++ Screens/AddCleanUp.js
@@ -1,16 +1,36 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Image, Button } from 'react-native'
 import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { Ionicons } from '@expo/vector-icons';
  
 
 const AddCleanUp = () => {
+  const route = useRoute();
+  const {spot} = route.params;
+  console.log(spot);
   const navigation = useNavigation();
   return (
     <View>
       <Header navigation={navigation}/>
       <Text>AddCleanUp</Text>
+      {spot &&  <View style={styles.headerWrapper}>
+                <Text>{spot.place} thani detail screen</Text>
+                <Image source={{uri:spot.titleImage}} style={{height:100, width:100}}/>
+                  
+                <Text >{spot.usermail}</Text>
+                <Text>{spot.seviority}</Text>
+                <Text>Food_Wrappers {spot.Food_Wrappers}</Text>
+                <Text>Polythene_bags {spot.Polythene_bags}</Text>
+                <Text>PET_Bottles {spot.PET_Bottles}</Text>
+                <Text>Plastic_Debris {spot.Plastic_Debris}</Text>
+                <Text>Large_Plastic_Rigid {spot.Large_Plastic_Rigid}</Text>
+                <View style={{bottom:0, marginBottom:5}}>
+                <Button title='Im cleaning it on ' color='green' onPress={()=>navigation.navigate('AddCleanUp',{spot})}/>
+                </View>
+            </View>
+    } 
     </View>
+    
   )
 }
 
