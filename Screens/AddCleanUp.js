8279d0a92722f66cfaea5da52bf9f1322b5adda9
Abishethvarman V <abishethvarman@gmail.com>
2022-03-30 00:07:20 +0530

    use route to the add cleanup page

--- Screens/AddCleanUp.js
+++ Screens/AddCleanUp.js
@@ -1,13 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Ionicons } from '@expo/vector-icons';
+ 
 
 const AddCleanUp = () => {
+  const navigation = useNavigation();
   return (
     <View>
+      <Header navigation={navigation}/>
       <Text>AddCleanUp</Text>
     </View>
   )
 }
 
+const Header = ({navigation: { goBack }})=>(
+  <View style={styles.headerContainer}>
+          <TouchableOpacity onPress={()=> goBack()}>
+            <Text></Text>
+          <Ionicons name="chevron-back-outline" size={30} color="blue" />
+          </TouchableOpacity>
+          <Text style={styles.headerText}>Insights Time</Text>
+          <Text> </Text>
+      </View>
+)
+
+const styles=StyleSheet.create({
+  headerText:{
+    color:'red'
+  }
+})
 export default AddCleanUp
 
